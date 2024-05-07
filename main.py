1c01b15337d872149f3f327e5476ea772169040e
Aydar <a.almazov2005@gmail.com>
2024-05-08 00:48:58 +0600

    final commit

--- main.py
+++ main.py
@@ -1,7 +1,30 @@
 import tkinter as tk
 from tkinter import *
 import pyttsx3
+engine=pyttsx3.init()
+def speaknow():
+  engine.say(Text.get())
+  engine.runAndWait()
+  engine.stop()
 
-root=Tk()
+root = tk.Tk()
+Text=StringVar()
 
+obj=LabelFrame(root, text= "Text to speech", font=20, bd=1)
+obj.pack(fill="both", expand="yes", padx=10,pady=10)
+
+lbl=Label(obj, text="Text", font=30)
+lbl.pack(side=tk.LEFT, padx=5)
+
+text=Entry(obj, textvariable=Text, font=30,width=25,bd=5)
+text.pack(side=tk.LEFT, padx=10)
+
+btn=Button(obj, text="Speak", font=20, bg="black",fg="white", command=speaknow)
+btn.pack(side=tk.LEFT, padx=10)
+
+
+root.title("Text to speech")
+root.geometry("480x240")
+root.resizable(False, False)
+root.mainloop()
 
